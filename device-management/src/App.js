fd664082120abff7d65c4ec1a8eb79c5659179d4
vshamily <108460464+vshamily@users.noreply.github.com>
2022-07-05 16:27:40 +0530

    Add files via upload
    Test commit

---
+++ device-management/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import DeviceStatusComponent from './components/DeviceStatusComponent'
+
+function App() {
+  return (
+    <React.Fragment>
+      <DeviceStatusComponent/>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ device-management/src/components/DeviceStatusComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DeviceStatusComponent = (props) => {
+    return (
+        <div> Code goes here </div>
+    )
+}
+
+export default DeviceStatusComponent;
